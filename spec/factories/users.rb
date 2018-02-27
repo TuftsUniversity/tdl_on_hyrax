9675552e408a336d977a4fd3787f847516da9514
Mike Korcynski <mkorcy@gmail.com>
2018-02-27 15:12:16 -0500

    adds specs for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,18 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
-    sequence :email do |n|                       # change :email back to :username when ldap is ready
-      "user#{User.count}_#{n}@tufts.edu"         # remove @tufts.edu when ldap is ready
+    sequence :email do |n|
+      "#{n}#{FFaker::Internet.email}"
+    end
+    sequence :username do |n|
+      "#{FFaker::Internet.user_name}#{n}"
     end
     password 'password'
+    display_name FFaker::Name.name
+    after(:create) { |user| user.remove_role(:admin) }
+
+    factory :admin do
+      after(:create) { |user| user.add_role(:admin) }
+    end
   end
 end
