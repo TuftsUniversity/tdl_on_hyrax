603122e31e22134af7e3bba3beb6170b3d9b0597
bgoodmon <brian.goodmon@tufts.edu>
2018-03-30 15:01:54 -0400

    Moved code from audio/video controllers to audio/video presenters where it belongs.  Transcript contents aren't done yet.

--- app/controllers/concerns/with_transcripts.rb
+++ app/controllers/concerns/with_transcripts.rb
@@ -9,9 +9,6 @@ module WithTranscripts
 
       @document_fedora = ActiveFedora::Base.find(params[:id])
       @document_tei = nil
-      @transcript_download_path = nil
-      @media_play_path = nil
-      @media_download_path = nil
 
       return unless @document_fedora.instance_of?(Audio) || @document_fedora.instance_of?(Video)
 
@@ -23,18 +20,9 @@ module WithTranscripts
         original_file = file_set.original_file
         unless original_file.nil?
           if original_file.mime_type == "text/xml"
-            @transcript_download_path = hyrax.download_path(file_set)
             @document_tei = Datastreams::Tei.from_xml(original_file.content)
             @document_tei.ng_xml.remove_namespaces! unless @document_tei.nil?
-          else
-            filename_extension = (@document_fedora.instance_of?(Audio) ? "mp3" : "mp4")
-            derivative_paths = Hyrax::DerivativePath.derivatives_for_reference(file_set)
-            derivative_paths.each do |derivative_path|
-              next unless derivative_path.end_with?(filename_extension)
-              @media_play_path = hyrax.download_path(file_set)
-              @media_download_path = hyrax.download_path(file_set, file: filename_extension)
-              break
-            end
+            break
           end
         end
       end
--- app/presenters/hyrax/audio_presenter.rb
+++ app/presenters/hyrax/audio_presenter.rb
@@ -2,5 +2,33 @@
 #  `rails generate hyrax:work Audio`
 module Hyrax
   class AudioPresenter < GenericWorkPresenter
+
+    def transcript_id
+      fsps = file_set_presenters
+
+      return if fsps.nil?
+
+      fsps.each do |fsp|
+        if fsp.mime_type == "text/xml"
+          return fsp.id
+        end
+      end
+    end
+
+    def media_id
+      fsps = file_set_presenters
+
+      return if fsps.nil?
+
+      fsps.each do |fsp|
+        unless fsp.mime_type == "text/xml"
+          derivative_paths = Hyrax::DerivativePath.derivatives_for_reference(fsp.id)
+          derivative_paths.each do |derivative_path|
+            return fsp.id if derivative_path.end_with?("mp3")
+          end
+        end
+      end
+    end
+
   end
 end
