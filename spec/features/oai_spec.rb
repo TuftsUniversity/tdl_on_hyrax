ca7c70f6c7a1d8c2c79d736fc0b97253cec01bcc
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-04-04 11:22:52 -0400

    Added oai-pmh server test.

---
+++ spec/features/oai_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'OAI-PMH' do
+  before do
+    FactoryBot.create(:tufts_MS999_ead)
+  end
+
+  # Our EAD fixture should be in the oai feed.
+  scenario 'Visit the OAI-PMH feed' do
+    visit URI.escape('/catalog/oai?verb=ListRecords&metadataPrefix=oai_dc')
+    expect(page.body).to include("test_aspace_ms999")
+    expect(page.body).to include("Lorem Ipsum papers, 1897-1933")
+  end
+end
