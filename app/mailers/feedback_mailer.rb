784a1c4d914aa278bc98bbe7d9029ecd48e827e1
bgoodmon <brian.goodmon@tufts.edu>
2018-04-18 12:30:55 -0400

    Changes for rubocop.

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,12 +1,11 @@
 class FeedbackMailer < ApplicationMailer
-  default :subject => "TDL Content Feedback"
+  default subject: "TDL Content Feedback"
 
   def feedback(params)
     @params = params
 
     # TBD - get "to" and "subject" from settings...
-    mail(:to => Rails.configuration.tdl_feedback_address,
-      :from => params[:email])
+    mail(to: Rails.configuration.tdl_feedback_address,
+         from: params[:email])
   end
-
 end
