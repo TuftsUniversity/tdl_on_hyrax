83afed711b51af91b210dc7e7845dd282e6594f9
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-05-22 13:06:37 -0400

    Fixed rubocop

--- spec/features/search_results_spec.rb
+++ spec/features/search_results_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 feature 'Search Results' do
-
   before(:all) do
     FactoryBot.create(:image1)
     FactoryBot.create(:image2)
   end
 
   scenario "Contributor shows only when there's no Creator" do
-    #visit '/catalog?f%5Bhuman_readable_type_sim%5D%5B%5D=Image'
     visit '/catalog?f[human_readable_type_sim][]=Image'
     expect(page).to have_text("Good Contrib")
     expect(page).not_to have_text("Bad Contrib")
