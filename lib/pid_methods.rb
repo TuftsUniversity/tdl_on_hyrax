fdfad5efea584831ec7c9e8fa12944c35d707edd
bgoodmon <brian.goodmon@tufts.edu>
2018-06-07 16:47:56 -0400

    Fixed ingested?() to deal with F4 pids properly.

--- lib/pid_methods.rb
+++ lib/pid_methods.rb
@@ -4,25 +4,20 @@ module PidMethods
     f4_id = ""
     thumbnail_path = ""
 
-    if pid.start_with?("tufts:")
-      solr_connection = ActiveFedora.solr.conn
-      fq = 'legacy_pid_tesim:"' + pid + '"'
+    fq = (pid.start_with?("tufts:") ? ('legacy_pid_tesim:"' + pid + '"') : ('id:"' + pid + '"'))
 
-      response = solr_connection.get 'select', params: { fq: fq, rows: '1' }
-      collection_length = response['response']['docs'].length
+    solr_connection = ActiveFedora.solr.conn
 
-      if collection_length > 0
-        result = true
-        f4_id = response['response']['docs'][0]['id']
-        thumbnail_path = response['response']['docs'][0]['thumbnail_path_ss']
-      end
-    else
-      begin
-        ActiveFedora::Base.load_instance_from_solr(pid)
-        result = true
-        f4_id = pid
-      rescue
-      end
+    response = solr_connection.get 'select', params: { fq: fq, rows: '1' }
+    collection_length = response['response']['docs'].length
+
+    if collection_length > 0
+      result = true
+      resp_doc = response['response']['docs'][0]
+      resp_id = resp_doc['id']
+      resp_thumbnail_path = resp_doc['thumbnail_path_ss']
+      f4_id = resp_id unless resp_id.nil?
+      thumbnail_path = resp_thumbnail_path unless resp_thumbnail_path.nil?
     end
 
     [result, f4_id, thumbnail_path]
