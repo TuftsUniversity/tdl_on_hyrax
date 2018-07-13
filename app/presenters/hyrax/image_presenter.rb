13bd80c9e37745fdb3b74ad764806bf65654fe15
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-07-13 14:37:00 -0400

    Fixed manifest_metadata to delete any blank fields, as those fields will prevent the metadata from appearing in the manifest.

--- app/presenters/hyrax/image_presenter.rb
+++ app/presenters/hyrax/image_presenter.rb
@@ -2,5 +2,17 @@
 #  `rails generate hyrax:work Image`
 module Hyrax
   class ImagePresenter < GenericWorkPresenter
+
+    # IIIF metadata for inclusion in the manifest
+    #  Called by the `iiif_manifest` gem to add metadata
+    #
+    # iiif_manifest doesn't allow for blank fields, so this
+    #   removes any of them before moving on.
+    #
+    # @return [Array] array of metadata hashes
+    def manifest_metadata
+      super.delete_if { |data| data["value"].blank? }
+    end
+
   end
 end
