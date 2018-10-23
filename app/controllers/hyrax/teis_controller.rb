dd38642a19fabff159735644683e9f5349a5a5ed
Mike Korcynski <mkorcy@gmail.com>
2018-10-23 14:58:05 -0400

    loads tei from fedora

--- app/controllers/hyrax/teis_controller.rb
+++ app/controllers/hyrax/teis_controller.rb
@@ -5,7 +5,21 @@ module Hyrax
   class TeisController < ApplicationController
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
+    include WithTeis
     self.curation_concern_type = ::Tei
     self.show_presenter = Hyrax::TeiPresenter
+
+    # TODO: Refactor this so that we're not loading from fedora
+    before_action :load_fedora_document
+
+    def advanced
+      @id = params[:id]
+      # or, to use "id" instead of "@id" in views, do this: render locals: {id:  params[:id]}
+
+      respond_to do |wants|
+        wants.html { presenter && parent_presenter }
+      end
+      render layout: "hyrax"
+    end
   end
 end
