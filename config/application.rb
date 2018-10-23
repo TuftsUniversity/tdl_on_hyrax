dcb35992064a19ff85bc7d6add923eebe26520cb
Mike Korcynski <mkorcy@gmail.com>
2018-10-23 14:56:57 -0400

    autoload lib/tufts

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module TdlOnHyrax
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib', 'tufts')
+
     # sitemap generator
     config.sitemap = {
       generate: true
