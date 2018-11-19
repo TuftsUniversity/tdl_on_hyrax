f038b7e0afebcad82e98b0c3650043469accb3c8
Mike Korcynski <mike.korcynski@tufts.edu>
2018-11-19 17:38:38 -0500

    enforce embargos correctly

---
+++ app/models/search_builder.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+class SearchBuilder < Hyrax::CatalogSearchBuilder
+  include Blacklight::Solr::SearchBuilderBehavior
+  include BlacklightRangeLimit::RangeLimitBuilder
+  # Add a filter query to restrict the search to documents the current user has access to
+  #  include Hydra::AccessControlsEnforcement
+  #  include Hyrax::SearchFilters
+  include BlacklightAdvancedSearch::AdvancedSearchBuilder
+  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr, :add_dl_filter, :suppress_embargo_records]
+
+  def add_dl_filter(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << 'displays_in_sim:dl'
+  end
+
+  # Override default behavior so admin users can see unpublished works in the search results
+  def suppress_embargo_records(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << '-suppressed_bsi:true'
+    solr_parameters[:fq] << '-embargo_release_date_dtsi:[NOW TO *]'
+  end
+
+  ##
+  # @example Adding a new step to the processor chain
+  #   self.default_processor_chain += [:add_custom_data_to_query]
+  #
+  #   def add_custom_data_to_query(solr_parameters)
+  #     solr_parameters[:custom] = blacklight_params[:user_value]
+  #   end
+end
