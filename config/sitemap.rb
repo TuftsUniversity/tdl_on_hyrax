8a7ef87d3cdec191d67bddfecabde20faee041c6
Mike Korcynski <mike.korcynski@tufts.edu>
2019-02-01 15:30:43 -0500

    sitemap had some repeated urls

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -13,48 +13,40 @@ SitemapGenerator::Sitemap.create do
   end
 
   # Add single record pages
-  cursor_mark = '*'
   repo = CatalogController.new.repository
-  loop do
-    response = repo.search('q' => 'displays_in_sim:dl',
-                           'qt' => 'search',
-                           'fq' => ['displays_in_sim:dl', '-suppressed_bsi:true', '{!terms f=has_model_ssim}Audio,Ead,GenericObject,Image,Pdf,Rcr,Tei,Video,VotingRecord,Collection', '({!terms f=edit_access_group_ssim}public) OR ({!terms f=discover_access_group_ssim}public) OR ({!terms f=read_access_group_ssim}public)', '({!terms f=edit_access_group_ssim}public) OR ({!terms f=discover_access_group_ssim}public) OR ({!terms f=read_access_group_ssim}public)'],
-                           'fl'         => 'id, has_model_ssim', # we only need the ids
-                           #   'cursorMark' => cursor_mark, # we need to use the cursor mark to handle paging
-                           'rows'       => ENV['BATCH_SIZE'] || 1_000_000,
-                           'sort'       => 'system_create_dtsi asc')
-
-    response['response']['docs'].each do |doc|
-      # /concern/images/
-      url = '/concern/'
-      model = doc['has_model_ssim'].present? ? doc['has_model_ssim'].first : "SKIP"
-      case  model
-      when "Image"
-        model = 'images/'
-      when "Pdf"
-        model = 'pdfs/'
-      when "Ead"
-        model = 'eads/'
-      when "Rcr"
-        model = 'rcrs/'
-      when "Audio"
-        model = 'audios/'
-      when "Video"
-        model = 'videos/'
-      when "GenericObject"
-        model = 'generic_objects/'
-      when "Tei"
-        model = 'teis/'
-      when "VotingRecord"
-        model = 'voting_records/'
-      else
-        next
-      end
-      add url + model + (doc['id']).to_s, changefreq: 'yearly', priority: 0.9
+  response = repo.search('q' => 'displays_in_sim:dl',
+                         'qt' => 'search',
+                         'fq' => ['displays_in_sim:dl', '-suppressed_bsi:true', '{!terms f=has_model_ssim}Audio,Ead,GenericObject,Image,Pdf,Rcr,Tei,Video,VotingRecord,Collection', '({!terms f=edit_access_group_ssim}public) OR ({!terms f=discover_access_group_ssim}public) OR ({!terms f=read_access_group_ssim}public)', '({!terms f=edit_access_group_ssim}public) OR ({!terms f=discover_access_group_ssim}public) OR ({!terms f=read_access_group_ssim}public)'],
+                         'fl'         => 'id, has_model_ssim', # we only need the ids
+                         #   'cursorMark' => cursor_mark, # we need to use the cursor mark to handle paging
+                         'rows'       => ENV['BATCH_SIZE'] || 1_000_000,
+                         'sort'       => 'system_create_dtsi asc')
+  response['response']['docs'].each do |doc|
+    # /concern/images/
+    url = '/concern/'
+    model = doc['has_model_ssim'].present? ? doc['has_model_ssim'].first : "SKIP"
+    case  model
+    when "Image"
+      model = 'images/'
+    when "Pdf"
+      model = 'pdfs/'
+    when "Ead"
+      model = 'eads/'
+    when "Rcr"
+      model = 'rcrs/'
+    when "Audio"
+      model = 'audios/'
+    when "Video"
+      model = 'videos/'
+    when "GenericObject"
+      model = 'generic_objects/'
+    when "Tei"
+      model = 'teis/'
+    when "VotingRecord"
+      model = 'voting_records/'
+    else
+      next
     end
-
-    break if response['nextCursorMark'] == cursor_mark # this means the result set is finished
-
-    cursor_mark = response['nextCursorMark']
+    add url + model + (doc['id']).to_s, changefreq: 'yearly', priority: 0.9
   end
 end
