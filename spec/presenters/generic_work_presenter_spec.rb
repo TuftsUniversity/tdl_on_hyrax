9f0d236ca0479b234bf92fe8d8cb9472fafa20cb
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-03-25 09:08:00 -0400

    Fixed tests

--- spec/presenters/generic_work_presenter_spec.rb
+++ spec/presenters/generic_work_presenter_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe Hyrax::GenericWorkPresenter do
   describe 'multi_attributes_to_html' do
-    let(doc) { SolrDocument.new }
-    let(user) { FactoryBot.build(:ldap_admin) }
-    let(ability) { Ability.new(user) }
+    let(:doc) { SolrDocument.new }
+    let(:user) { FactoryBot.build(:ldap_admin) }
+    let(:ability) { Ability.new(user) }
 
     it 'returns nothing if there are no fields' do
       presenter = described_class.new(doc, ability)
