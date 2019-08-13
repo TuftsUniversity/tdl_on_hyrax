fb4e817090ad2ab0f02c920eb7d5886ecf93c1a2
Mike Korcynski <mike.korcynski@tufts.edu>
2019-08-13 11:51:40 -0400

    handle published state for viewing pdfs

--- app/controllers/pdfviewer_controller.rb
+++ app/controllers/pdfviewer_controller.rb
@@ -2,5 +2,11 @@ class PdfviewerController < ApplicationController
   layout "pdfviewer"
   def index
     @id = params[:id]
+    @parent = params[:parent]
+    obj = ActiveFedora::Base.find(@parent, cast: true)
+    published = true
+    published =  !obj.state.id.to_s.include?('inactive') unless obj.state.nil?
+    redirect_to "/concern/pdfs/#{params[:parent]}" unless published
+
   end
 end
