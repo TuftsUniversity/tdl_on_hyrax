255dd49f16c6cd74d8c9538afdc45338f21fedb7
bgoodmon <brian.goodmon@tufts.edu>
2020-03-09 10:22:58 -0400

    Meaningless whitespace changes to make rubocop happy.

--- app/controllers/pdfviewer_controller.rb
+++ app/controllers/pdfviewer_controller.rb
@@ -5,8 +5,7 @@ class PdfviewerController < ApplicationController
     @parent = params[:parent]
     obj = ActiveFedora::Base.find(@parent, cast: true)
     published = true
-    published =  !obj.state.id.to_s.include?('inactive') unless obj.state.nil?
+    published = !obj.state.id.to_s.include?('inactive') unless obj.state.nil?
     redirect_to "/concern/pdfs/#{params[:parent]}" unless published
-
   end
 end
