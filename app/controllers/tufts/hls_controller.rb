ed870fbcaaf0475ce80e204f19252c3213d35e4c
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-10-14 11:10:28 -0400

    HLS working locally. Time to test on dev

--- app/controllers/tufts/hls_controller.rb
+++ app/controllers/tufts/hls_controller.rb
@@ -1,32 +1,27 @@
+# Tufts created controller for providing HLS files to mediaelement player.
 module Tufts
-  class HlsController
-    include Hyrax::LocalFileDownloadsControllerBehavior
-
+  class HlsController < Hyrax::DownloadsController
     def show
-      return '' if params[:file].empty?
-
-      if(params[:file] == 'm3u8')
-        file = path_to_derivs(true)
-      else
-        file = path_to_derivs + "/#{params[:file]}"
-      end
-      byebug
-
-      file
+      send_local_content
+    rescue
+      raise ActiveFedora::ObjectNotFoundError
     end
 
-    def path_to_derivs(find_m3u8 = false)
-      full_path = Hyrax::FileSetDerivativesService.new(file_set).derivative_url('m3u8')
-      if(find_m3u8)
-        full_path
-      else
+    private
+
+      # Overwriting Hyrax::DownloadBehavior.load_file
+      def load_file
+        full_path = Hyrax::FileSetDerivativesService.new(file_set).derivative_url('m3u8')
         path_pieces = full_path.split('/')
-        path_pieces.pop
+        path_pieces[0] = '' # Remove file:/
+        if params[:file] != 'm3u8'
+          path_pieces[-1] = params[:file] # Replace p-m3u8.m3u8 file with .ts file, if supplied in request.
+        end
+        path_pieces.join('/')
       end
-    end
 
-    def file_set
-      @file_set ||= FileSet.find(params[:id])
-    end
+      def file_set
+        @file_set ||= FileSet.find(params[:id])
+      end
   end
 end
