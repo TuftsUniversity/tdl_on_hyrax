5c12692355698fe78b2ffe974c9f230c9c874c08
Mike Korcynski <mkorcy@gmail.com>
2021-05-10 13:26:05 -0400

    trying to narrow down

--- spec/factories/ead.rb
+++ spec/factories/ead.rb
@@ -17,9 +17,9 @@ FactoryBot.define do
     end
 
     after(:create) do |work, _evaluator|
-      ead_test_file_1 = File.open(File.expand_path(File.join(Rails.root.to_s, "spec", "fixtures", "MS999.archival.xml")))
-      original_file = work.file_sets[0].build_original_file
-      original_file.content = ead_test_file_1
+      # ead_test_file_1 = File.open(File.expand_path(File.join(Rails.root.to_s, "spec", "fixtures", "MS999.archival.xml")))
+      # original_file = work.file_sets[0].build_original_file
+      # original_file.content = ead_test_file_1
       #  work.file_sets[0].save
       work.save
     end
@@ -43,9 +43,9 @@ FactoryBot.define do
     end
 
     after(:create) do |work, _evaluator|
-      ead_test_file_1 = File.open(File.expand_path(File.join(Rails.root.to_s, "spec", "fixtures", "MS226.archival.xml")))
-      original_file = work.file_sets[0].build_original_file
-      original_file.content = ead_test_file_1
+      # ead_test_file_1 = File.open(File.expand_path(File.join(Rails.root.to_s, "spec", "fixtures", "MS226.archival.xml")))
+      # original_file = work.file_sets[0].build_original_file
+      # original_file.content = ead_test_file_1
       #  work.file_sets[0].save
       work.save
     end
