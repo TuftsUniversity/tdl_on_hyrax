a6b238ac735a861fa80da2b8c109b9319894f299
Mike Korcynski <mkorcy@gmail.com>
2021-05-20 09:53:02 -0400

    adds full text searching support

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -6,7 +6,7 @@ class SearchBuilder < Hyrax::CatalogSearchBuilder
   #  include Hydra::AccessControlsEnforcement
   #  include Hyrax::SearchFilters
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
-  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr, :add_dl_filter, :suppress_embargo_records, :supress_eads_conditionally]
+  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr, :add_dl_filter, :suppress_embargo_records, :supress_eads_conditionally, :show_works_or_works_that_contain_files]
 
   def supress_eads_conditionally(solr_parameters)
     solr_parameters[:fq] ||= []
@@ -25,11 +25,31 @@ class SearchBuilder < Hyrax::CatalogSearchBuilder
     solr_parameters[:fq] << '-embargo_release_date_dtsi:[NOW TO *]'
   end
 
-  ##
-  # @example Adding a new step to the processor chain
-  #   self.default_processor_chain += [:add_custom_data_to_query]
-  #
-  #   def add_custom_data_to_query(solr_parameters)
-  #     solr_parameters[:custom] = blacklight_params[:user_value]
-  #   end
+  def show_works_or_works_that_contain_files(solr_parameters)
+    return if blacklight_params[:q].blank? || blacklight_params[:search_field] != 'all_fields'
+    solr_parameters[:user_query] = blacklight_params[:q]
+    solr_parameters[:q] = new_query
+  end
+
+  private
+
+    # the {!lucene} gives us the OR syntax
+    def new_query
+      "{!lucene}#{interal_query(dismax_query)} #{interal_query(join_for_works_from_files)}"
+    end
+
+    # the _query_ allows for another parser (aka dismax)
+    def interal_query(query_value)
+      "_query_:\"#{query_value}\""
+    end
+
+    # the {!dismax} causes the query to go against the query fields
+    def dismax_query
+      "{!dismax v=$user_query}"
+    end
+
+    # join from file id to work relationship solrized file_set_ids_ssim
+    def join_for_works_from_files
+      "{!join from=#{ActiveFedora.id_field} to=file_set_ids_ssim}#{dismax_query}"
+    end
 end
