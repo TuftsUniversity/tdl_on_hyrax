3cec2afe330c9c2298542a73baacfe1281ac1d87
Travis Lilleberg <travis.lilleberg@tufts.edu>
2021-08-23 10:29:07 -0400

    Fixed aeon specs by fixing ids

--- spec/features/visitor_uses_aeon_spec.rb
+++ spec/features/visitor_uses_aeon_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 feature 'Visitor goes to homepage' do
-  #    before(:each) do
-  #      @session = Capybara::Session.new(Capybara.javascript_driver)
-  #    end
   before do
     visit "/" # This can be whatever URL you need it to be
     page.execute_script("localStorage.clear()")
@@ -16,7 +13,7 @@ feature 'Visitor goes to homepage' do
   end
 
   scenario 'adds item to list', js: true do
-    visit '/concern/eads/test_aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
+    visit '/concern/eads/aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
     expect(page).to have_content "Fitzgerald, Zelda 1910-1923"
     click_link "Add to List"
     click_link 'My List'
@@ -24,7 +21,7 @@ feature 'Visitor goes to homepage' do
   end
 
   scenario 'adds item to list, and requests copies', js: true do
-    visit '/concern/eads/test_aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
+    visit '/concern/eads/aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
     click_link "Add to List"
     click_link 'My List'
     click_button "Request Copies"
@@ -32,7 +29,7 @@ feature 'Visitor goes to homepage' do
   end
 
   scenario 'adds item to list, and requests reading room visit', js: true do
-    visit '/concern/eads/test_aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
+    visit '/concern/eads/aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
     click_link "Add to List"
     click_link 'My List'
     click_button "Request in Reading Room"
@@ -40,7 +37,7 @@ feature 'Visitor goes to homepage' do
   end
 
   scenario 'adds item to list, and saves in TASCR for later use', js: true do
-    visit '/concern/eads/test_aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
+    visit '/concern/eads/aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
     click_link "Add to List"
     click_link 'My List'
     click_button "Save in TASCR"
@@ -48,7 +45,7 @@ feature 'Visitor goes to homepage' do
   end
 
   scenario 'adds item to list, and then removes all items from list', js: true do
-    visit '/concern/eads/test_aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
+    visit '/concern/eads/aspace_ms999/fa/aspace_5fc68062788bbf01180b4812d1d5d5cf'
     click_link "Add to List"
     click_link 'My List'
     page.find_button("Remove all Items from List").trigger('click')
