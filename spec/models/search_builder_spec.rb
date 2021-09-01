3d4c52c9d949ae68c3c9cd6d885e1131cad139b6
Travis Lilleberg <travis.lilleberg@tufts.edu>
2021-09-01 11:02:39 -0400

    Fixed Rspec/VerifiedDoubles issue

--- spec/models/search_builder_spec.rb
+++ spec/models/search_builder_spec.rb
@@ -5,7 +5,7 @@ describe SearchBuilder do
   subject(:search_builder) { described_class.new scope }
   let(:user_params) { {} }
   let(:blacklight_config) { Blacklight::Configuration.new }
-  let(:scope) { double blacklight_config: blacklight_config }
+  let(:scope) { instance_double('CatalogController', blacklight_config: blacklight_config) }
 
   describe '#default_processor_chain' do
     it 'adds display filter for dl to processing chain' do
