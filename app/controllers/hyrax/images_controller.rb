7fb26bc1c180e5dd72f37a2840e70e4b33b2a47e
Mike Korcynski <mkorcy@gmail.com>
2021-09-21 16:55:17 -0400

    removes unneeded code

--- app/controllers/hyrax/images_controller.rb
+++ app/controllers/hyrax/images_controller.rb
@@ -9,16 +9,6 @@ module Hyrax
     self.curation_concern_type = ::Image
     self.show_presenter = Hyrax::ImagePresenter
 
-    ###
-    # Adds thumbnails to the IIIF Manifest for images.
-    def manifest
-      headers['Access-Control-Allow-Origin'] = '*'
-
-      manifest_json = manifest_builder.to_h
-      manifest_json['thumbnail'] = { '@id' => thumbnail }
-      render json: manifest_json
-    end
-
     def advanced
       @id = params[:id]
       # or, to use "id" instead of "@id" in views, do this: render locals: {id:  params[:id]}
@@ -28,14 +18,5 @@ module Hyrax
       end
       render layout: "imageviewer"
     end
-
-    private
-
-      ###
-      # Gets the external thumbnail path of a resource.
-      def thumbnail
-        thumbnail_path = Hyrax::ThumbnailPathService.call(@presenter).slice(1..-1)
-        root_url + thumbnail_path
-      end
   end
 end
