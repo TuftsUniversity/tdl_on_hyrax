322ac80663c48843352ae66d7768699a17b71c5d
Travis Lilleberg <travis.lilleberg@tufts.edu>
2021-09-22 13:47:33 -0400

    Added randomly chosen banners to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  ##
+  # A banner image from app/assets/images/banners, chosen at random
+  #
+  # @return {str}
+  #   A string, 'banners/***', for use in image_tag()
+  def random_banner
+    banners = Dir[Rails.root.join('app', 'assets', 'images', 'banners', '*')]
+    "banners/#{File.basename(banners.sample, '.*')}"
+  end
 end
