1e358984b745f7170cf11d2631a6d0aaab5b03b4
Travis Lilleberg <travis.lilleberg@tufts.edu>
2021-10-08 10:35:50 -0400

    Finished basic blocks for front page that display images (with alt tags), titles, and links to content. No animations or descriptions yet

--- app/helpers/featured_collections_helper.rb
+++ app/helpers/featured_collections_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module FeaturedCollectionsHelper
-  def featured_blocks
+  def featured_collections
     file = Rails.root.join('config', 'featured_collections.yml').to_s
     YAML.safe_load(File.open(file))&.each(&:deep_symbolize_keys!)
   end
