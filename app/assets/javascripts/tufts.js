3bf7a601a78956cbdd7387d1c974c2e68b4f7bcf
Travis Lilleberg <travis.lilleberg@tufts.edu>
2022-02-22 10:30:59 -0500

    Added click and touch events for featured_collection boxes to flip them if user doesn't have access to hoverr events

--- app/assets/javascripts/tufts.js
+++ app/assets/javascripts/tufts.js
@@ -5,7 +5,7 @@
    This script manually sets the height of the element to the height of the image + collection title.
    And it does so on window resize as well, so the height is always correct.
  */
-(function tuftsScope() {
+(function tuftsHeightScope() {
   "use strict";
   let set_fc_heights, is_homepage;
 
@@ -32,5 +32,36 @@
     if(!is_homepage()) { return }
     setTimeout(set_fc_heights, 250); // For some reason, this still can fire too early, even after DomContentLoaded.
     window.addEventListener('resize', set_fc_heights);
+  });
+})();
+
+
+/* Sets click and touch events to manually flip the featured collection cards, for people who can't hover. */
+(function tuftsClickTouchScope() {
+  "use strict";
+  let transform_value = 'rotateY(180deg)', flip_card, is_homepage;
+
+  // Flips the cards. Doesn't work when hover is active.
+  flip_card = function flipCard(e) {
+    if(e.currentTarget.style.transform === transform_value) {
+      e.currentTarget.style.transform = '';
+    } else {
+      e.currentTarget.style.transform = transform_value;
+    }
+  }
+
+  // Looks for the homepage class on the body tag.
+  is_homepage = function isHomepage() {
+    return document.querySelector('body').classList.contains('homepage')
+  }
+
+  window.addEventListener('DOMContentLoaded', function() {
+    let fc = document.querySelectorAll('.featured_collection > .flipper');
+    if(!is_homepage() || fc.length === 0) { return }
+
+    fc.forEach(function(el) {
+      el.addEventListener('click', flip_card);
+      el.addEventListener('touchstart', flip_card);
+    });
   });
 })();
