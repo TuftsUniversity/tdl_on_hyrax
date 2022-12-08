514744c66abdc781f60d35f5ee443e6bc26787a3
Ryan Orlando <rynorlando@gmail.com>
2022-12-08 11:33:04 -0500

    Add a test to make sure thesis type is use as a label instead of dissertation type

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe CatalogController, type: :controller do
   it "uses the dl search filter" do
     expect(catalog_controller.search_builder.processor_chain).to include :add_dl_filter
   end
+
+  it 'uses "Thesis Type" as a label isntead of "Dissertation Type"' do
+  	config = ::CatalogController.blacklight_config
+  	puts config
+    vistit '/catalog', { :use_route  => :rails_admin }
+    expect(response).to be_successful
+    expect(response).to include 'Thesis Type'
+  end
+
 end
