29702dd0f76d5250b83ce2d2a7868431734cd924
Brian Goodmon <brian.goodmon@gmail.com>
2023-01-23 15:45:24 -0500

    Work in progress - audio spec test checks text on page, catches 2243 bug.
    Removed archival .wav file - it's too big for git and wasn't needed
    anyway.

---
+++ spec/features/audio_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+feature 'Audio' do
+  before(:all) do
+    FactoryBot.create(:tufts_MS123_audio)
+  end
+
+  scenario 'View MS123 (Interview with Horace Works) page' do
+    visit '/concern/audios/8910jt5bg'
+    expect(page).to have_text 'Interview with Horace Works'
+    expect(page).to have_text 'Participants'
+    expect(page).to have_text 'HW'
+    expect(page).to have_text 'Horace Works, interviewee (male)'
+    expect(page).to have_text 'KC'
+    expect(page).to have_text 'Kenneth J. Cleary, interviewer (male)'
+    expect(page).to have_text 'Information'
+    expect(page).to have_text 'Transcript'
+    expect(page).to have_text 'view transcript only'
+    expect(page).to have_text 'Can you, can you talk about, can you give any examples of how he motivated you to continue a season when you lost 25 games?'
+  end
+end
