5d703c917e5fb39aeb84091bf43cac553e697c18
Brian Goodmon <brian.goodmon@gmail.com>
2023-02-03 18:40:05 -0500

    Added more testing scenarios.

--- spec/features/audio_spec.rb
+++ spec/features/audio_spec.rb
@@ -6,14 +6,14 @@ feature 'Audio' do
     FactoryBot.create(:tufts_MS123_audio)
   end
 
-  scenario 'View MS123 (Interview with Horace Works) page', js: true do
+  scenario 'View MS123 (Interview with Horace Works) page' do
     visit '/concern/audios/1234jt5bg'
     expect(page).to have_text 'Interview with Horace Works'
     expect(page).to have_text 'DCA Citation Guide'
     expect(page).to have_text 'Participants'
   end
 
-  scenario 'View MS123 (Interview with Horace Works) participants', js: true do
+  scenario 'View MS123 (Interview with Horace Works) participants' do
     visit '/concern/audios/1234jt5bg'
     expect(page).to have_text 'HW'
     expect(page).to have_text 'Horace Works, interviewee (male)'
@@ -21,22 +21,39 @@ feature 'Audio' do
     expect(page).to have_text 'Kenneth J. Cleary, interviewer (male)'
   end
 
-  scenario 'View MS123 (Interview with Horace Works) information tab', js: true do
+  scenario 'View MS123 (Interview with Horace Works) information tab visibility' do
     visit '/concern/audios/1234jt5bg'
     expect(page).to have_link 'Information', href: '#information'
+    click_link 'Information'
+    expect(page).to have_selector('#information', visible: true)
+    expect(page).to have_selector('#transcript',  visible: false)
+  end
+
+  scenario 'View MS123 (Interview with Horace Works) information tab content' do
+    visit '/concern/audios/1234jt5bg'
+    expect(page).to have_link 'Information', href: '#information'
+    click_link 'Information'
     expect(page).to have_text 'Description'
     expect(page).to have_text 'Interview conducted by Kenneth J. Cleary.'
   end
 
-  scenario 'View MS123 (Interview with Horace Works) transcript tab', js: true do
+  scenario 'View MS123 (Interview with Horace Works) transcript tab visibility' do
     visit '/concern/audios/1234jt5bg'
     expect(page).to have_link 'Transcript', href: '#transcript'
     click_link 'Transcript'
-    expect(page).to have_text 'Can you, can you talk about, can you give any examples of how he motivated you to continue a season when you lost 25 games?'
+    expect(page).to have_selector('#transcript',  visible: true)
+    expect(page).to have_selector('#information', visible: false)
   end
 
-  scenario 'View MS123 (Interview with Horace Works) transcript only page', js: true do
+  scenario 'View MS123 (Interview with Horace Works) transcript tab content' do
     visit '/concern/audios/1234jt5bg'
+    expect(page).to have_link 'Transcript', href: '#transcript'
+    click_link 'Transcript'
+    expect(page).to have_text 'can you give any examples of how he motivated you'
+  end
+
+  scenario 'View MS123 (Interview with Horace Works) transcript only page' do
+    visit '/concern/audios/1234jt5bg#transcript'
     expect(page).to have_link 'view transcript only', href: '/concern/audios/1234jt5bg/transcriptonly'
     click_link 'view transcript only'
     expect(page).to have_text 'So the city made a deal with me.'
