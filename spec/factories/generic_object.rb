2de84d221bd94da8166d74902dbed7ba15dc87ab
Mike Korcynski <mkorcy@gmail.com>
2023-03-07 16:22:08 -0500

    WIP

--- spec/factories/generic_object.rb
+++ spec/factories/generic_object.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 FactoryBot.define do
-  factory :generic_object_1, class: GenericObject do
+  factory :generic_object_with_xml, class: ::GenericObject do
     transient do
       user { FactoryBot.create(:user) } # find_or_create ???
     end
@@ -18,10 +18,37 @@ FactoryBot.define do
     end
 
     after(:create) do |work, _evaluator|
-      tei_test_file1 = File.open(File.expand_path(Rails.root.join('spec', 'fixtures', 'MS123.001.001.00002.tei.xml')))
+      tei_test_file = File.open(File.expand_path(Rails.root.join('spec', 'fixtures', 'MS123.001.001.00002.tei.xml')))
       original_file = work.file_sets[0].build_original_file
       original_file.mime_type = 'text/xml'
-      original_file.content = tei_test_file1
+      original_file.content = tei_test_file
+      work.file_sets[0].save
+      work.save
+    end
+  end
+
+  factory :generic_object_with_mp3, class: ::GenericObject do
+    transient do
+      user { FactoryBot.create(:user) } # find_or_create ???
+    end
+    id { '782466dgh' }
+    title { ["Unknown Object"] }
+    displays_in { ['dl'] }
+    visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+
+    before(:create) do |work, evaluator|
+      work.ordered_members << create(:file_set, user: evaluator.user, title: ['A Contained FileSet'], id: "782466dfs")
+    end
+
+    after(:build) do |work, evaluator|
+      work.apply_depositor_metadata(evaluator.user.user_key)
+    end
+
+    after(:create) do |work, _evaluator|
+      tei_test_file = File.open(File.expand_path(Rails.root.join('spec', 'fixtures', 'MS123.001.001.00002.mp3')))
+      original_file = work.file_sets[0].build_original_file
+      original_file.mime_type = 'none/non'
+      original_file.content = tei_test_file
       work.file_sets[0].save
       work.save
     end
--- spec/helpers/hyrax_helper_spec.rb
+++ spec/helpers/hyrax_helper_spec.rb
@@ -6,13 +6,22 @@ describe HyraxHelper do
   # include ActionView::Helpers
   include described_class
 
-  FactoryBot.create(:generic_object_1)
+  before do
+    FactoryBot.create(:generic_object_with_xml)
+    FactoryBot.create(:generic_object_with_mp3)
+  end
 
   # rubocop:disable RSpec/InstanceVariable
-  it "gets a generic link" do
+  it "gets a generic link for a known mimetype" do
     allow(@presenter).to receive(:id).and_return('8910jt5bg')
     object = ActiveFedora::Base.find('8910jt5bg')
     file_set = object.file_sets[0]
     expect(generic_link(file_set.id)).to eq({ icons: "glyphicon glyphicon-file glyph-left", label: "Download File", text: "Download File", url: "/downloads/8910jt5fs?filename=8910jt5bg.xml" })
+  end
+
+  # rubocop:disable RSpec/InstanceVariable
+  it "gets a generic link for a unknown mimetype" do
+    allow(@presenter).to receive(:id).and_return('782466dgh')
+    expect(generic_link('782466dfs')).to eq({ icons: "glyphicon glyphicon-file glyph-left", label: "Download File", text: "Download File", url: "/downloads/782466dfs?filename=782466dgh.non" })
   end
 end
