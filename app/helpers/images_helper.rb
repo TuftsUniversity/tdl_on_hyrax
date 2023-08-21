7140c1bfeee44ab9dc27c360458f43ea5d93d6a2
Ryan Orlando <rynorlando@gmail.com>
2023-08-21 13:50:00 -0400

    Fix Image show error

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module ImagesHelper
+  # rubocop:disable Metrics/CyclomaticComplexity
   def get_rotation(image)
-    return 0 if image.orientation.nil?
+    return 0 if image.nil? || image.orientation.nil?
+    return 0 unless image.orientation.is_a? Enumerable
+    return 0 unless image.orientation[0].is_a? String
 
     logger.warn("An image mirroring was not handled: " + image.id) if image_mirrored? image
     extract_rotation_from_orientation(image) || 0
@@ -13,7 +16,6 @@ private
   def extract_rotation_from_orientation(image)
     rotation = image.orientation[0].gsub(/[^0-9]/, '')
     return rotation.to_i unless rotation.empty?
-    nil
   end
 
   def image_mirrored?(image)
