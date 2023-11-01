154ec7a1feab24ec24098d465c57f05738c71ea1
Mike Korcynski <mkorcy@gmail.com>
2023-11-01 11:06:19 -0400

    adds some specs

---
+++ spec/presenters/hyrax/rcr_presenter_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Hyrax::RcrPresenter do
+  let(:presenter) { described_class.new(double, nil) }
+
+  describe '#rcr_id' do
+    context 'when file_set_presenters returns nil' do
+      before do
+        allow(presenter).to receive(:file_set_presenters).and_return(nil)
+      end
+
+      it 'returns nil' do
+        expect(presenter.rcr_id).to be_nil
+      end
+    end
+
+    context 'when there is a file_set_presenter with mime_type "text/xml"' do
+      let(:xml_fsp) { double(id: '123', mime_type: 'text/xml') }
+
+      before do
+        allow(presenter).to receive(:file_set_presenters).and_return([xml_fsp])
+      end
+
+      it 'returns the id of the xml file_set_presenter' do
+        expect(presenter.rcr_id).to eq('123')
+      end
+    end
+
+    context 'when there is no file_set_presenter with mime_type "text/xml"' do
+      let(:other_fsp) { double(id: '456', mime_type: 'text/plain') }
+
+      before do
+        allow(presenter).to receive(:file_set_presenters).and_return([other_fsp])
+      end
+
+      it 'returns nil' do
+        expect(presenter.rcr_id).to be_nil
+      end
+    end
+  end
+end
